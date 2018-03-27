2b4d73a3bf6550c0aa1020a29b14d922bc65a7ca
SaturdayAM <jwl62@georgetown.edu>
2018-03-27 12:31:43 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,75 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
+  end
+
+  def self.first_X_students_in_grade_10(n)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = 10
+      LIMIT ?;
+    SQL
+    DB[:conn].execute(sql, n).map{|row| self.new_from_db(row)}
   end
   
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1).first
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE students.grade = ?;
+    SQL
+
+    DB[:conn].execute(sql, x).map {|row| self.new_from_db(row)}
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = 9;
+    SQL
+    DB[:conn].execute(sql).map{|row|self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade < 12;
+    SQL
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
@@ -32,7 +89,6 @@ class Student
       grade TEXT
     )
     SQL
-
     DB[:conn].execute(sql)
   end
 
